12b7e53167151896b0893defe749d12336728782
Matt Colf <mattcolf@mattcolf.com>
2012-01-10 13:28:43 -0500

    Added readme. Changed class name.

---
+++ README.md
@@ -0,0 +1,41 @@
+#Simple Key:Value Flat File Storage Class
+
+This class provides an interface to a very simple key:value based storage system. All data is 
+serialized and saved to a flat file. 
+
+##FEATURES
+
+- Provides a key:value storage system. Supports most PHP objects including multidimensional arrays.
+
+##DEPENDENCIES
+
+- Apache must have R+W access to the storage.php.serial file.
+
+##USAGE
+
+1. Move the storage folder into your web area.
+2. Link the class into your existing project.
+``````````````````````````
+require_once "storage/simple_storage.class.php";
+``````````````````````````
+3. Create an instance of the class.
+``````````````````````````
+$storage = new SimpleStorage();
+``````````````````````````
+4. Put and get content as needed. Note that the storage key must be a string!.
+``````````````````````````
+$book = array(
+	"title" => "A Day In The Life",
+	"author" => "John Smith",
+	"date" => date("c"),
+	"pages" => 428,
+	"contents" => array(
+		"chapter1" => "One upon a time...",
+		"chapter2" => "...a toad...",
+		"chapter3" => "found a home in the forest.".
+	)
+);
+$storage->put("book",$book);
+$stored_book = $storage->get("book");
+print_r($stored_book);
+``````````````````````````
---
+++ storage/simple_storage.class.php
@@ -0,0 +1,61 @@
+<?php
+
+// very simple flat file storage system
+
+class SimpleStorage {
+
+	private $file = "storage.php.serial";
+	private $data = array("updated" => "", "checksum" => "", "data" => array("foo" => "bar"));
+	private $dirty = FALSE;
+	
+	public function __construct() {
+		// load data
+		if ( file_exists($this->file) ) {
+			$serial = file_get_contents($this->file);
+			if ( strlen($serial) == 0 ) return;
+			if ( ($this->data = unserialize($serial)) === FALSE ) throw new Exception("Unable to unserialize $this->file.");
+			// verify data integrity
+			if ( $this->data["checksum"] != md5($this->data["data"]) ) throw new Exception("Data from $this->file is not valid. Fails checksum.");
+			// import data
+			if ( ($this->data["data"] = unserialize($this->data["data"])) === FALSE ) throw new Exception("Unable to unserialize data from $file.");	
+		}
+	}
+	
+	public function __destruct() {
+		// flush data
+		$this->flush();
+	}
+	
+	public function flush() {
+		// check if writeback is needed
+		if ( $this->dirty == FALSE ) return TRUE;
+		// prepare to writeback to file
+		$data = $this->data;
+		$data["updated"] = date("c");;
+		$data["data"] = serialize($this->data["data"]);
+		$data["checksum"] = md5($data["data"]);
+		// overwrite existing data
+		if ( file_put_contents($this->file,serialize($data)) ) return TRUE;
+		else throw new Exception("Unable to write back to $this->file. Data will be lost.");
+	}
+
+	public function put($key,$value) {
+		if ( is_string($key) ) {
+			$this->data["data"][$key] = $value;
+			$this->dirty = TRUE;
+			return TRUE;
+		} else return FALSE;
+	}
+	
+	public function get($key) {
+		if ( is_string($key) ) {
+			if ( array_key_exists($key,$this->data["data"]) ) {
+				return $this->data["data"][$key];
+			}	
+		}
+		return FALSE;
+	}
+
+}
+
+?>
